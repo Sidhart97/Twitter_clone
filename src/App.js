3767e0dc4f7f7185ade113b8034c79635416c1ee
Sidhart97 <sidharth.sivasree@gmail.com>
2022-09-19 00:31:37 +0530

    twitter

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import "./App.css";
+
+function App() {
+  return (
+    // BEM
+    <div className="app">
+      <Sidebar />
+     
+    </div>
+  );
+}
+
+export default App;
